532870f600090dffb31dacfd855e245e4d23be4e
dnx2k <denis.khoruzhiy@bankex.co>
2017-10-14 19:03:56 -0400

    Gamplay significantly changed

--- frontend/public/js/scripts.js
+++ frontend/public/js/scripts.js
@@ -1,8 +1,18 @@
+var address;
 var ethBalance = 123;
-var plasmaBalance = 0;
+
+var heroes = [
+	{ title: 'Wonder Woman', desc: '1941', img: 'wonder-woman.jpg' },
+	{ title: 'Superman', desc: '1938', img: 'superman.jpg' },
+	{ title: 'Green Lantern', desc: '1940', img: 'green-lantern.jpg' },
+	{ title: 'Batman', desc: '1939', img: 'batman.jpg' },
+];
+
+var collectedHeroes = [];
 
 $(document).ready(function () {
 	updateBalances();
+	getAccountAddress();
 
 	$('#btn-deposit').click(function () {
 		$('#no-eth-transactions').addClass('hidden');
@@ -29,48 +39,73 @@ $(document).ready(function () {
 						+ moment().format("MM/DD/YYYY HH:mm:SS") + 
 					'</div>\
 				</div>\
-				<div class="col">\
-					<div class="text">\
-						0x0000000000000000000000000000000000000001\
+				<div class="col-md-auto">\
+					<div class="text text-right">\
+						0.1&nbsp;<span class="text-muted">ETH</span> <i class="fa fa-arrow-circle-right"></i>\
 					</div>\
 				</div>\
-				<div class="col-md-auto">\
-					<div class="text">\
-						<i class="fa fa-arrow-circle-down"></i> 0.1&nbsp;<span class="text-muted">ETH</span>\
-					</div>\
+			</div>\
+			<div class="row">\
+				<div class="col">\
+					<div class="text text-sm">'
+					+ address +
+					'</div>\
 				</div>\
 			</div>');
 
 			$("#no-plasma-assets").addClass("hidden");
 
-			var ulPlasma = $("#list-plasma-assets");
-			ulPlasma.removeClass("hidden");
-
-			var liPlasma = $('<li class="list-group-item list-group-item-warning" style="display: none;">\
-				<div class="row">\
-					<div class="col-md-auto">\
-						<div class="text text-muted">\
-							10/14/2017 17:00:00\
-						</div>\
-					</div>\
-					<div class="col">\
-						<div class="text">\
-							0x0000000000000000000000000000000000000001\
-						</div>\
-					</div>\
-					<div class="col-md-auto">\
-						<button type="button" class="btn btn-info btn-block" id="btn-send-offchain" onclick="onSend(this)"><i class="fa fa-paper-plane"></i> Send</button>\
-					</div>\
-					<div class="col-md-auto">\
-						<button type="button" class="btn btn-success btn-block" id="btn-withdraw" onclick="onWithdraw(this)"><i class="fa fa-arrow-circle-up"></i> Withdraw</button>\
-					</div>\
-				</div>\
-			</li>');
-
-			liPlasma.appendTo(ulPlasma).fadeIn(500);
+			buyHeroes(1);
 
 			ethBalance -= 1;
-			plasmaBalance += 1;
+			updateBalances();
+		}, 1500);
+	});
+
+	$('#btn-deposit-3').click(function () {
+		$('#no-eth-transactions').addClass('hidden');
+
+		var ul = $('#list-eth-transactions');
+		ul.removeClass('hidden');
+		
+		var li = $('<li class="list-group-item list-group-item-primary" style="display: none;">\
+			<div class="row justify-content-md-center">\
+				<div class="col-md-auto">\
+					<div class="text">\
+						<i class="fa fa-spinner fa-spin fa-fw"></i>\
+					</div>\
+				</div>\
+			</div>\
+		</li>');
+
+		li.appendTo(ul).fadeIn(500);
+
+		setTimeout(function () {
+			li.html('<div class="row">\
+				<div class="col-md-auto">\
+					<div class="text text-muted">'
+						+ moment().format("MM/DD/YYYY HH:mm:SS") + 
+					'</div>\
+				</div>\
+				<div class="col-md-auto">\
+					<div class="text text-right">\
+						0.3&nbsp;<span class="text-muted">ETH</span> <i class="fa fa-arrow-circle-right"></i>\
+					</div>\
+				</div>\
+			</div>\
+			<div class="row">\
+				<div class="col">\
+					<div class="text text-sm">'
+					+ address +
+					'</div>\
+				</div>\
+			</div>');
+
+			$("#no-plasma-assets").addClass("hidden");
+
+			buyHeroes(3);
+
+			ethBalance -= 3;
 			updateBalances();
 		}, 1500);
 	});
@@ -78,9 +113,52 @@ $(document).ready(function () {
 	$('.btn-withdraw').click(onWithdraw);
 });
 
+function getHero() {
+	return heroes[Math.floor(Math.random() * heroes.length)];
+}
+
+function buyHeroes(count) {
+	var collectionPlasma = $("#list-plasma-assets");
+	collectionPlasma.removeClass("hidden");
+
+	for (var i = 0; i < count; i++) {
+		var hero = getHero();
+
+		if (collectedHeroes.indexOf(hero.title) === -1) {
+			collectedHeroes.push(hero.title);
+			updateBalances();
+		}
+
+		var cardPlasma = $('<div class="col-md-3">\
+			<div class="card hero">\
+				<img class="card-img-top" src="/img/heroes/' + hero.img +'" alt="">\
+				<div class="card-body">\
+					<h5 class="card-title">' + hero.title + '</h5>\
+					<p class="card-text">' + hero.desc + '</p>\
+					<div class="row">\
+						<div class="col-sm-4">\
+							<button type="button" class="btn btn-sm btn-success btn-block" id="btn-withdraw" onclick="onWithdraw(this)"><i class="fa fa-arrow-circle-left"></i></button>\
+						</div>\
+						<div class="col-sm-8">\
+							<button type="button" class="btn btn-sm btn-info btn-block" id="btn-send-offchain" onclick="onExchange(this)"><i class="fa fa-refresh"></i> Exchange</button>\
+						</div>\
+					</div>\
+				</div>\
+			</div>\
+		</div>');
+
+		cardPlasma.appendTo(collectionPlasma).fadeIn(500);
+	}
+}
+
 function updateBalances() {
 	$("#eth-balance").text((ethBalance / 10.0).toFixed(1));
-	$("#plasma-balance").text((plasmaBalance / 10.0).toFixed(1));
+	$("#heroes-count").text(collectedHeroes.length);
+	$("#heroes-total").text(heroes.length);
+
+	if (heroes.length == collectedHeroes.length) {
+		$('#my-cards').addClass('bg-success');
+	}
 }
 
 function onWithdraw(sender) {
@@ -103,8 +181,8 @@ function onWithdraw(sender) {
 
 	li.appendTo(ul).fadeIn(500);
 
-	var liPlasma = $(sender).parents('li');
-	liPlasma.addClass('disabled');
+	var cardPlasma = $(sender).parents('li');
+	cardPlasma.addClass('disabled');
 
 	setTimeout(function () {
 		li.html('<div class="row">\
@@ -125,39 +203,38 @@ function onWithdraw(sender) {
 			</div>\
 		</div>');
 
-		liPlasma.fadeOut(500, function () {
-			liPlasma.remove();
+		cardPlasma.fadeOut(500, function () {
+			cardPlasma.remove();
 
-			var ulPlasma = $("#list-plasma-assets");
+			var collectionPlasma = $("#list-plasma-assets");
 
-			if (ulPlasma.children().length === 0)
+			if (collectionPlasma.children().length === 0)
 				$("#no-plasma-assets").removeClass("hidden");
 		});
 
 		ethBalance += 1;
-		plasmaBalance -= 1;
 		updateBalances();
 	}, 1500);
 }
 
-function onSend(sender) {
+function onExchange(sender) {
 	$("#no-plasma-assets-others").addClass("hidden");
 
-	var liPlasma = $(sender).parents('li');
-	liPlasma.addClass('disabled');
-	liPlasma.fadeOut(500, function () {
-		liPlasma.remove();
+	var cardPlasma = $(sender).parents('li');
+	cardPlasma.addClass('disabled');
+	cardPlasma.fadeOut(500, function () {
+		cardPlasma.remove();
 
-		var ulPlasma = $("#list-plasma-assets");
+		var collectionPlasma = $("#list-plasma-assets");
 
-		if (ulPlasma.children().length === 0)
+		if (collectionPlasma.children().length === 0)
 			$("#no-plasma-assets").removeClass("hidden");
 	});
 
-	var ulPlasmaOthers = $("#list-plasma-assets-others");
-	ulPlasmaOthers.removeClass("hidden");
+	var collectionPlasmaOthers = $("#list-plasma-assets-others");
+	collectionPlasmaOthers.removeClass("hidden");
 
-	var liPlasmaOthers = $('<li class="list-group-item list-group-item-secondary" style="display: none;">\
+	var cardPlasmaOthers = $('<li class="list-group-item list-group-item-secondary" style="display: none;">\
 		<div class="row justify-content-md-center">\
 			<div class="col-md-auto">\
 				<div class="text">\
@@ -167,10 +244,10 @@ function onSend(sender) {
 		</div>\
 	</li>');
 
-	liPlasmaOthers.appendTo(ulPlasmaOthers).fadeIn(500);
+	cardPlasmaOthers.appendTo(collectionPlasmaOthers).fadeIn(500);
 	
 	setTimeout(function () {
-		liPlasmaOthers.html('<div class="row">\
+		cardPlasmaOthers.html('<div class="row">\
 			<div class="col-md-auto">\
 				<div class="text text-muted">\
 					10/14/2017 17:00:00\
@@ -183,7 +260,27 @@ function onSend(sender) {
 			</div>\
 		</div>');
 
-		plasmaBalance -= 1;
 		updateBalances();
 	}, 300);
+}
+
+function getAccountAddress() {
+	setTimeout(function () {
+		if (typeof web3 !== 'undefined') {
+			web3.version.getNetwork(function (err, netId) {
+				if (netId === "4") { //Rinkeby
+					web3.eth.getAccounts(function (err, accounts) {
+						address = accounts[0];
+						$("#eth-address").text(address);
+					});
+				}
+				else {
+					alert("Please enable Rinkeby network");
+				}
+			});
+		}
+		else {
+			alert("Please install MetaMask");
+		}
+	}, 100);
 }
